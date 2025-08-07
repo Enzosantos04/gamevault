13b23884d2839a5a4e2cfe5cfaaa779bcb6485af
enzosdev <enzohfs@gmail.com>
2025-08-07 10:50:43 -0300

    feat:cria entidade user que representa a tabela user no banco de dados

--- src/main/java/com/gamevault/gamevault/entity/User.java
+++ src/main/java/com/gamevault/gamevault/entity/User.java
@@ -1,4 +1,78 @@
 package com.gamevault.gamevault.entity;
 
-public class User {
+import com.gamevault.gamevault.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private UserRole roles;
+    @CreationTimestamp
+    private Date created_at;
+    @UpdateTimestamp
+    private Date updated_at;
+    @Column(nullable = false)
+    private String url_img;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // se o usuário for admin, retorna as authorities de admin e user
+        // se for user, retorna apenas a authority de user
+        if(this.roles == UserRole.ADMIN)
+            //esse return instancia as authorities do usuário
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+            else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
