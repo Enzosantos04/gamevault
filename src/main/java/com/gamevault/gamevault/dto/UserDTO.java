d6a45cb0b5b18e847b660886d7a738c2cb1df462
enzosdev <enzohfs@gmail.com>
2025-08-07 10:55:04 -0300

    feat: userDTO

--- src/main/java/com/gamevault/gamevault/dto/UserDTO.java
+++ src/main/java/com/gamevault/gamevault/dto/UserDTO.java
@@ -1,4 +1,19 @@
 package com.gamevault.gamevault.dto;
 
+
+import com.gamevault.gamevault.enums.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private UserRole roles;
+    private String url_img;
 }
