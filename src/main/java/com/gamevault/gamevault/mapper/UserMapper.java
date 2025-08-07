8b54089ed97c1ee32796422392d815c9073e0d1a
enzosdev <enzohfs@gmail.com>
2025-08-07 10:56:37 -0300

    feat: mapper para traduzir os datos do UserDTO

--- src/main/java/com/gamevault/gamevault/mapper/UserMapper.java
+++ src/main/java/com/gamevault/gamevault/mapper/UserMapper.java
@@ -1,4 +1,27 @@
 package com.gamevault.gamevault.mapper;
 
+import com.gamevault.gamevault.dto.UserDTO;
+import com.gamevault.gamevault.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserMapper {
+    public User map(UserDTO dto) {
+        User user = new User();
+        user.setId(dto.getId());
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setUrl_img(dto.getUrl_img());
+        user.setRoles(dto.getRoles());
+        return user;
+    }
+   public UserDTO map(User user){
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        dto.setPassword(user.getPassword());
+        return dto;
+   }
 }
