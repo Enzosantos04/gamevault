4f55e1135b59f92d5415f4d4b3402952093cfc55
enzosdev <enzohfs@gmail.com>
2025-08-07 10:45:21 -0300

    feat: adiciona regra de negocio a entidade user, metodos registe e login

--- src/main/java/com/gamevault/gamevault/service/UserService.java
+++ src/main/java/com/gamevault/gamevault/service/UserService.java
@@ -1,4 +1,30 @@
 package com.gamevault.gamevault.service;
 
+import com.gamevault.gamevault.dto.UserDTO;
+import com.gamevault.gamevault.entity.User;
+import com.gamevault.gamevault.mapper.UserMapper;
+import com.gamevault.gamevault.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserDTO register(UserDTO userDTO){
+    User user = userMapper.map(userDTO);
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        User savedUser = userRepository.save(user);
+        return userMapper.map(savedUser);
+
+}
 }
