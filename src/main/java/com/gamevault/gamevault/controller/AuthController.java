a1968686d43433c5b5c75f879194a4a8e555e9b3
enzosdev <enzohfs@gmail.com>
2025-08-07 10:15:13 -0300

    feat: cria endpoints para registro de usuario e login

--- src/main/java/com/gamevault/gamevault/controller/AuthController.java
+++ src/main/java/com/gamevault/gamevault/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.gamevault.gamevault.controller;
 
+
+import com.gamevault.gamevault.dto.UserDTO;
+import com.gamevault.gamevault.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gamevault/auth")
 public class AuthController {
+
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register(UserDTO userDTO) {
+        UserDTO savedUser = userService.register(userDTO);
+        return ResponseEntity.ok(userDTO);
+    }
 }
