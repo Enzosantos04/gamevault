ed2b4effaa1564415ddd028951b747b02f1870d1
enzosdev <enzohfs@gmail.com>
2025-08-09 18:45:32 -0300

    feat: metodo que busca o user no banco de dados pelo email, se esse email nao estiver cadastrado vai lancar uma exception UserNotFound

--- src/main/java/com/gamevault/gamevault/service/AuthService.java
+++ src/main/java/com/gamevault/gamevault/service/AuthService.java
@@ -1,4 +1,23 @@
 package com.gamevault.gamevault.service;
 
-public class AuthService {
+import com.gamevault.gamevault.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found"));
+    }
 }
