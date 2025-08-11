a0a7c77b3bb315410292edb4f1e63ff393b5b4bd
enzosdev <enzohfs@gmail.com>
2025-08-11 20:49:32 -0300

    feat: Record que armazenam os dados que vao no payload do Token JWT

--- src/main/java/com/gamevault/gamevault/config/JWTUserData.java
+++ src/main/java/com/gamevault/gamevault/config/JWTUserData.java
@@ -3,5 +3,6 @@ package com.gamevault.gamevault.config;
 import lombok.Builder;
 
 @Builder
-public record JWTUserData(Long id, String email, String roles) {
+
+public record JWTUserData(Long id, String name, String email, String roles) {
 }
