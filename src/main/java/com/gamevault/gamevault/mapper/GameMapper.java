3b50b37cde8053b544c3bbbcacf58025ab365be6
enzosdev <enzohfs@gmail.com>
2025-08-12 20:02:27 -0300

    feat: Mapper para traduzir o DTO da entidade Game

--- src/main/java/com/gamevault/gamevault/mapper/GameMapper.java
+++ src/main/java/com/gamevault/gamevault/mapper/GameMapper.java
@@ -1,7 +1,28 @@
 package com.gamevault.gamevault.mapper;
 
+import com.gamevault.gamevault.dto.GameDTO;
+import com.gamevault.gamevault.entity.Game;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameMapper {
+
+    public Game map(GameDTO dto) {
+        Game game = new Game();
+        game.setId(dto.getId());
+        game.setTitle(dto.getTitle());
+        game.setDescription(dto.getDescription());
+        game.setReleaseDate(dto.getReleaseDate());
+        return game;
+    }
+
+
+    public GameDTO map(Game game){
+        GameDTO dto = new GameDTO();
+        dto.setId(game.getId());
+        dto.setTitle(game.getTitle());
+        dto.setDescription(game.getDescription());
+        dto.setReleaseDate(game.getReleaseDate());
+        return dto;
+    }
 }
