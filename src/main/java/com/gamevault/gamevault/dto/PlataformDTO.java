39554e6e6ded67eb65b5b37efef3f30be79a0a2b
enzosdev <enzohfs@gmail.com>
2025-08-12 09:13:19 -0300

    refactor: Adiciona Getter e Setter para poder ter PlataformMapper

--- src/main/java/com/gamevault/gamevault/dto/PlataformDTO.java
+++ src/main/java/com/gamevault/gamevault/dto/PlataformDTO.java
@@ -1,8 +1,31 @@
 package com.gamevault.gamevault.dto;
 
-public record PlataformDTO(String name) {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-    public PlataformDTO (String name) {
+@Builder
+public record PlataformDTO(String name, Long id) {
+
+
+
+    public PlataformDTO(String name, Long id) {
         this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
     }
 }
