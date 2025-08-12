407ae48e6e71ee6fcd09a438e75a83d52e295950
enzosdev <enzohfs@gmail.com>
2025-08-12 09:17:15 -0300

    feat: Mapper para o PlataformDTO

--- src/main/java/com/gamevault/gamevault/mapper/PlataformMapper.java
+++ src/main/java/com/gamevault/gamevault/mapper/PlataformMapper.java
@@ -1,4 +1,23 @@
 package com.gamevault.gamevault.mapper;
 
+import com.gamevault.gamevault.dto.PlataformDTO;
+import com.gamevault.gamevault.entity.Plataform;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PlataformMapper {
+
+    public Plataform map(PlataformDTO dto){
+        Plataform plataform = new Plataform();
+        plataform.setId(plataform.getId());
+        plataform.setName(plataform.getName());
+        return plataform;
+    }
+
+    public PlataformDTO map(Plataform plataform){
+        PlataformDTO dto = new PlataformDTO(plataform.getName(), plataform.getId());
+        dto.setId(plataform.getId());
+        dto.setName(plataform.getName());
+        return dto;
+    }
 }
