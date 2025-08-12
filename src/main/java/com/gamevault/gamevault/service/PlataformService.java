c6799ecf6334816e74f7340abb4351f9e3f3dc60
enzosdev <enzohfs@gmail.com>
2025-08-12 10:27:59 -0300

    feat: Adiciona ao service o metodo getAllPlataforms

--- src/main/java/com/gamevault/gamevault/service/PlataformService.java
+++ src/main/java/com/gamevault/gamevault/service/PlataformService.java
@@ -1,4 +1,28 @@
 package com.gamevault.gamevault.service;
 
+import com.gamevault.gamevault.dto.PlataformDTO;
+import com.gamevault.gamevault.entity.Plataform;
+import com.gamevault.gamevault.mapper.PlataformMapper;
+import com.gamevault.gamevault.repository.PlataformRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class PlataformService {
+    private final PlataformRepository plataformRepository;
+    private final PlataformMapper plataformMapper;
+
+    public PlataformService(PlataformRepository plataformRepository, PlataformMapper plataformMapper) {
+        this.plataformMapper = plataformMapper;
+        this.plataformRepository = plataformRepository;
+    }
+
+    public List<PlataformDTO> getAllPlataforms() {
+      List<Plataform> movies = plataformRepository.findAll();
+        return movies.stream()
+                .map(plataformMapper::map)
+                .collect(Collectors.toList());
+    }
 }
