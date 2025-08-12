3c3629055a38b5392ea5e50dcf70df7bb88b9250
enzosdev <enzohfs@gmail.com>
2025-08-11 21:04:23 -0300

    feat: Add veryToken Method on TokenService

--- src/main/java/com/gamevault/gamevault/config/TokenService.java
+++ src/main/java/com/gamevault/gamevault/config/TokenService.java
@@ -3,11 +3,14 @@ package com.gamevault.gamevault.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.gamevault.gamevault.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -24,6 +27,24 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("GameVault")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            DecodedJWT jwt = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(jwt.getClaim("UserId").asLong())
+                    .name(jwt.getClaim("name").asString())
+                    .email(jwt.getSubject())
+                    .roles(jwt.getClaim("roles").asString())
+            .build());
+        }catch (JWTVerificationException exception){
+            return Optional.empty();
+        }
     }
 
 }
