e63363795eb353b63c54aa0b774242caf11e924f
enzosdev <enzohfs@gmail.com>
2025-08-12 17:52:47 -0300

    refactor: Cria se um DTO padrao usando classe inves de Record

--- src/main/java/com/gamevault/gamevault/dto/PlataformDTO.java
+++ src/main/java/com/gamevault/gamevault/dto/PlataformDTO.java
@@ -1,4 +1,13 @@
 package com.gamevault.gamevault.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlataformDTO {
+    private Long id;
+    private String name;
 }
