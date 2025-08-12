1a29e1054cb9a9f7e4baf907f138b18238eacdf0
enzosdev <enzohfs@gmail.com>
2025-08-12 08:50:59 -0300

    feat: Cria PlataformController sem os Endpoints ainda

--- src/main/java/com/gamevault/gamevault/controller/PlataformController.java
+++ src/main/java/com/gamevault/gamevault/controller/PlataformController.java
@@ -1,4 +1,15 @@
 package com.gamevault.gamevault.controller;
 
+import com.gamevault.gamevault.dto.PlataformDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gamevault/plataform")
 public class PlataformController {
+
+
 }
