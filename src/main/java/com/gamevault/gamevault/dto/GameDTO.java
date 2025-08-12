d36e2c10ce6662cca40ed8a39a4c160ac3160b46
enzosdev <enzohfs@gmail.com>
2025-08-12 19:59:54 -0300

    feat: Cria DTO para entidade Game

--- src/main/java/com/gamevault/gamevault/dto/GameDTO.java
+++ src/main/java/com/gamevault/gamevault/dto/GameDTO.java
@@ -1,11 +1,23 @@
 package com.gamevault.gamevault.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class GameDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private double rating;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "ddd/MM/yyyy")
+    private LocalDate releaseDate;
+    private String imageUrl;
+
+
 }
