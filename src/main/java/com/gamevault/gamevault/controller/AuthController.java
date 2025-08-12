56a066861b5c93910852af35c4ee3e80771c5ab1
enzosdev <enzohfs@gmail.com>
2025-08-11 21:28:02 -0300

    refactor: Adiciona no ResponseEntity HttpStatus.CREATED no endpoint Register

--- src/main/java/com/gamevault/gamevault/controller/AuthController.java
+++ src/main/java/com/gamevault/gamevault/controller/AuthController.java
@@ -8,6 +8,7 @@ import com.gamevault.gamevault.entity.User;
 import com.gamevault.gamevault.exception.UsernameOrPasswordInvalidException;
 import com.gamevault.gamevault.service.UserService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -33,7 +34,7 @@ public class  AuthController {
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO) {
         UserDTO savedUser = userService.register(userDTO);
-        return ResponseEntity.ok(savedUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
     @PostMapping("/login")
