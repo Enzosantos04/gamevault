949ab1a4704a0fdd4bc4a64a431c6c4299333dde
enzosdev <enzohfs@gmail.com>
2025-08-12 21:27:21 -0300

    fix: Corrige o pattern de data dentro da annotation JsonFormat

--- src/main/java/com/gamevault/gamevault/dto/GameDTO.java
+++ src/main/java/com/gamevault/gamevault/dto/GameDTO.java
@@ -1,6 +1,7 @@
 package com.gamevault.gamevault.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,8 +16,9 @@ public class GameDTO {
     private String title;
     private String description;
     private double rating;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "ddd/MM/yyyy")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate releaseDate;
+    @JsonProperty("imageUrl")
     private String imageUrl;
 
 
