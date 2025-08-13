47fc63890454339f99c5fe33338aa36d9a965585
enzosdev <enzohfs@gmail.com>
2025-08-12 21:22:07 -0300

    refactor: Adiciona Collections com GrantedAuthority

--- src/main/java/com/gamevault/gamevault/config/JWTUserData.java
+++ src/main/java/com/gamevault/gamevault/config/JWTUserData.java
@@ -1,8 +1,26 @@
 package com.gamevault.gamevault.config;
 
+import com.gamevault.gamevault.enums.UserRole;
 import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 @Builder
+public record JWTUserData(Long id, String name, String email, int roles) {
 
-public record JWTUserData(Long id, String name, String email, String roles) {
+    // Método para obter as authorities do usuário baseado no seu papel
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRole userRole = UserRole.values()[roles];
+        if (userRole == UserRole.ADMIN) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER")
+            );
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
 }
