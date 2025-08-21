f4f5cf68186fe44f380686798541e9f2beea7e61
enzosdev <enzohfs@gmail.com>
2025-08-21 12:31:52 -0300

    feat: add cors on auth endpoint

--- src/main/java/com/gamevault/gamevault/controller/AuthController.java
+++ src/main/java/com/gamevault/gamevault/controller/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/gamevault/auth")
+@CrossOrigin(origins = "http://localhost:5173/", allowedHeaders = "*")
 public class  AuthController {
 
 
